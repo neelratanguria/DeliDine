86e1ac0bceafc8bef0e27b835bd8bdbd31092be7
neelratanguria <neelratan@live.com>
2023-06-11 20:04:55 +0530

    CMS schema

--- sanity/schemas/category.js
+++ sanity/schemas/category.js
@@ -2,18 +2,19 @@ import {defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'category',
-  title: 'Category',
+  title: 'Menu Category',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    }),
+    {
+      name: "name",
+      title: "Category name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "image",
+      title: "Image Of Category",
+      type: "image",
+    },
   ],
 })
